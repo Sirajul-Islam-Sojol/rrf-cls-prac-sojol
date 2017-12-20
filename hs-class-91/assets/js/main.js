538621e4fd167392a9a10947c6173f27e9919001
Sirajul-Islam-Sojol <mdsirajul182@gmail.com>
2017-12-20 14:27:50 +0600

    hs-class-91

--- hs-class-91/assets/js/main.js
+++ hs-class-91/assets/js/main.js
@@ -1,6 +1,12 @@
 jQuery(document).ready(function($) {
 	$(".slider-area").owlCarousel({
-		item:1,
+        items: 1,
+        loop:true,
+        nav:false,
+        dots:true,
+        autoplay:false
+        
+        
 
         
         
