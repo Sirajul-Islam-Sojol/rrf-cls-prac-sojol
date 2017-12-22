42f52ba06524316074c038ae1c46cb75afe386bb
Sirajul-Islam-Sojol <mdsirajul182@gmail.com>
2017-12-22 23:56:16 +0600

    hs-class-96

---
+++ hs-class-96/assets/js/main.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function($) {
+	$(".slider-area").owlCarousel({
+        items: 1,
+        loop:true,
+        nav:false,
+        dots:true,
+        autoplay:false
+        
+        
+
+        
+        
+	
+		
+	});
+    $(".logo-carousel").owlCarousel({
+        items: 5,
+        loop:true,
+        nav:false,
+        dots:false,
+        autoplay:false,
+        margin:30,
+        
+        
+
+        
+        
+	
+		
+	});
+});
